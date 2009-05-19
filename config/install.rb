f4110f6f0b0ecaad54b9f299fb7cc77a64f653b6
Richard Luther <richard.luther@gmail.com>
2009-05-18 17:01:34 -0700

    More tweaks

--- config/install.rb
+++ config/install.rb
@@ -8,11 +8,11 @@ end
 # Build up your own and strip down your server until you get it right. 
 policy :passenger_stack, :roles => :app do
   requires :webserver               # Apache
-
+  
   requires :apache_etag_support     # == Apache extras
   requires :apache_deflate_support  # Read about these specialties in 
   requires :apache_expires_support  # stack/apache.rb
-
+  
   requires :appserver               # Passenger
   requires :ruby_enterprise         # Ruby Enterprise edition
   requires :database                # MySQL
@@ -22,9 +22,9 @@ policy :passenger_stack, :roles => :app do
   requires :scm                     # Git and subversion
   requires :memcached               # Memcached
   requires :libmemcached            # Libmemcached
-  requires :server_tweaks           # Misc server tweaks
   requires :firewall                # UFW  
   requires :misc                    # Misc
+  requires :server_tweaks           # Misc server tweaks
 end
 
 deployment do
--- config/stack/tweaks.rb
+++ config/stack/tweaks.rb
@@ -1,10 +1,12 @@
 package :server_tweaks do
   description 'Misc tweaks to the server'
 
-  push_text 'export PS1="\[\e[32;1m\]\u\[\e[0m\]\[\e[32m\]@\h\[\e[36m\]\w \[\e[33m\]\$ \[\e[0m\]"', '~/.bash_profile'
-  
-  post :install, 'sudo perl -pi -e \'s/(PermitRootLogin)\s+yes$/\1 no/g\' /etc/ssh/sshd_config'
-  post :install, 'sudo perl -pi -e \'s/(Port)\s+22$/\1 222/g\' /etc/ssh/sshd_config'
-  post :install, 'sudo /etc/init.d/ssh reload'
-  post :install, 'sudo adduser deploy'
+  push_text 'export PS1="\[\e[32;1m\]\u\[\e[0m\]\[\e[32m\]@\h\[\e[36m\]\w \[\e[33m\]\$ \[\e[0m\]"', '~/.bash_profile',  {:sudo => true} do
+    post :install, "echo 'hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii'"
+    # post :install, 'sudo perl -pi -e \'s/(PermitRootLogin)\s+yes$/\1 no/g\' /etc/ssh/sshd_config'
+    post :install, "sudo perl -p -i.old -e 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config"
+    post :install, "sudo perl -p -i.old -e 's/Port 22/Port 222/g' /etc/ssh/sshd_config"
+    # post :install, 'sudo perl -pi -e \'s/(Port)\s+22$/\1 222/g\' /etc/ssh/sshd_config'
+    post :install, 'sudo /etc/init.d/ssh reload'
+  end
 end
