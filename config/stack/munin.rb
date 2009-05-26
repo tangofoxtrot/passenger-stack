7c45b671c6ba52965a46db35377c9d2465dba2dd
Richard Luther <richard.luther@gmail.com>
2009-05-26 15:36:30 -0700

    Munin passenger scripts

--- config/stack/munin.rb
+++ config/stack/munin.rb
@@ -4,6 +4,15 @@ package :munin, :provides => :monitoring do
     post :install, 'htpasswd -cmb /etc/apache2/access.htpasswd user some_password'
     post :install, 'touch /etc/apache2/sites-available/munin.conf'
 
+    post :install, "curl http://gist.github.com/raw/118319/4ba7d81c29e10fb86e67d9a539ab5716704a61fc/Munin%20Passenger%20Memory%20Stats -o /etc/munin/plugins/passenger-memory"
+    post :install, "curl http://gist.github.com/raw/118319/d293dc236800a1cf02b56fc8c0eeb617d7a3455c/Munin%20Passenger%20Status -o /etc/munin/plugins/passenger-status"
+    post :install, "chmod a+x /etc/munin/plugins/passenger-status"
+    post :install, "chmod a+x /etc/munin/plugins/passenger-memory"
+    post :install, 'sudo perl -p -i.old -e \'s/PATH=\/sbin:\/bin:\/usr\/sbin:\/usr\/bin/PATH=\/sbin:\/bin:\/usr\/sbin:\/usr\/bin:\/usr\/local\/bin/g\' /etc/init.d/munin-node'
+    post :install, "echo 'nobody ALL=(ALL) NOPASSWD:/usr/local/bin/passenger-status, /usr/local/bin/passenger-memory-stats' |sudo tee -a /etc/sudoers"
+    post :install, "/etc/init.d/munin-node restart"
+
+
     [%q(<VirtualHost *:80>),
       %q(  ServerName munin.server.com),
       %q(  DocumentRoot /var/www/munin),
--- config/stack/tweaks.rb
+++ config/stack/tweaks.rb
@@ -3,7 +3,7 @@ package :server_tweaks do
 
   push_text 'export PS1="\[\e[32;1m\]\u\[\e[0m\]\[\e[32m\]@\h\[\e[36m\]\w \[\e[33m\]\$ \[\e[0m\]"', '~/.bash_profile',  {:sudo => true} do
     post :install, "echo 'EDITOR=vim' |sudo tee -a ~/.bash_profile"
-    post :install, "sudo perl -p -i.old -e 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config"
+    post :install, "sudo perl -p -i.old -e 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config"
     post :install, "sudo perl -p -i.old -e 's/Port 22/Port 222/g' /etc/ssh/sshd_config"
     # post :install, 'sudo perl -pi -e \'s/(Port)\s+22$/\1 222/g\' /etc/ssh/sshd_config'
     post :install, 'sudo /etc/init.d/ssh reload'
