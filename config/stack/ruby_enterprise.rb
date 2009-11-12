a5d3846c3587cd76fcfdc95b7374b380865548fa
Richard Luther <luther@imac-tango.local>
2009-11-12 09:37:29 -0800

    Updated REE and libmemcache versions

--- config/stack/ruby_enterprise.rb
+++ config/stack/ruby_enterprise.rb
@@ -1,10 +1,10 @@
 package :ruby_enterprise do
   description 'Ruby Enterprise Edition'
-  version '1.8.7-20090928'
+  version '1.8.7-2009.10'
   
   install_path = "/usr/local/ruby-enterprise"
   binaries = %w(erb gem irb passenger-config passenger-install-apache2-module passenger-make-enterprisey passenger-memory-stats passenger-spawn-server passenger-status passenger-stress-test rackup rails rake rdoc ree-version ri ruby testrb)
-  source "http://rubyforge.org/frs/download.php/64475/ruby-enterprise-1.8.7-20090928.tar.gz" do
+  source "http://rubyforge.org/frs/download.php/66162/ruby-enterprise-1.8.7-2009.10.tar.gz" do
     custom_install 'sudo ./installer --auto=/usr/local/ruby-enterprise'
     
     binaries.each {|bin| post :install, "ln -s #{install_path}/bin/#{bin} /usr/local/bin/#{bin}" }
